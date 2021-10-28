766231fb304bffc0fd8dc4e975219178b5c2a888
Emrys <s.hsdust@outlook.com>
2021-10-28 19:04:43 +0800

    154 & 540

--- source/154_minimum_in_rotated_sorted_arr.cpp
+++ source/154_minimum_in_rotated_sorted_arr.cpp
@@ -15,20 +15,27 @@ using namespace std;
 class Solution{
 public:
     int findMin(vector<int>& nums){
-        int l(0), r(nums.size()-1), mid, minn(2147483647);
-        while(l <= r){
-            mid = l + (r-l)/2;
-            if(nums[mid] < minn)
-                minn = nums[mid];
-
+        int l(0), r(nums.size()-1), mid;
+        while(l<=r){
+            int l(0), r(nums.size()-1), mid;
+            while(l < r){
+                mid = l + (r - l) / 2;
+                if(nums[mid] < nums[r])
+                    r = mid-1;
+                else if(nums[mid] > nums[r])
+                    l = mid;
+                else
+                    --r;
+            }
+            return nums[l];
         }
-        return minn;
+        return l;
     }
 };
 
 int main(){
-    int arr[20] = {1, 3, 5};
-    vector<int> nums(arr, arr+3);
+    int arr[20] = {1};
+    vector<int> nums(arr, arr+1);
 
     Solution s;
     cout << s.findMin(nums) << endl;
